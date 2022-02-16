438b6cc06ffac770ff750c3d03e671d130681a84
Rad Lisowski <radlisowski@gmail.com>
2022-02-16 23:08:18 +0000

    workking finally !

--- cypress/integration/login-test.spec.js
+++ cypress/integration/login-test.spec.js
@@ -4,7 +4,7 @@
 describe(`Verify that all the labels and controls including text-boxes, buttons, and links are present on the Login page and can be interacted with.`, () => {
     
     beforeEach(() => {
-        cy.visit('https://radlisowski.github.io//login-test-page/index.html')
+        cy.visit('https://radlisowski.github.io/login.html')
         
         cy.eyesOpen({
             appName: 'Login Page',
@@ -15,10 +15,12 @@ describe(`Verify that all the labels and controls including text-boxes, buttons,
     afterEach(() => {
         cy.eyesClose()
     });
+
+    //********tests-cases*********
     
     it(`LP01-Verifying visibility of the elements on the page` , ()=> {
         cy.eyesCheckWindow({
-            tag: "Login Page",
+            tag: "LP01-Verifying visibility of the elements on the page",
             target: 'window',
             fully: true
         });
@@ -37,13 +39,13 @@ describe(`Verify that all the labels and controls including text-boxes, buttons,
     })
 
     it(`LP04-Verifying Login button can be pressed` , ()=> {
-        cy.get('#login-form-submit').click()
+        cy.get('#signin-button').click()
 
-        cy.get('#login-error-msg').should('have.text', 'Invalid username and/or password')    
+        cy.get('#error-button').should('have.text', 'Wrong Username or Password')    
     })
-})
+ })
 
-// describe(`Verify various user interaction on the Login Page (positive/negative).`, () => {
+// describe(`LP05Verify various user interaction on the Login Page (positive/negative).`, () => {
     
 //     beforeEach(() => {
 //         cy.visit('https://radlisowski.github.io//login-test-page/index.html')
@@ -58,18 +60,20 @@ describe(`Verify that all the labels and controls including text-boxes, buttons,
 //         cy.eyesClose();
 //     });
 
-//     it(`LP08-Verify that as soon as the login page opens, by default the cursor should remain on the username textbox.` , ()=> {
+//     //********tests-cases*********
+
+//     it(`LP06-Verify that as soon as the login page opens, by default the cursor should remain on the username textbox.` , ()=> {
 //         cy.focused().should('have.id', 'username-field');
 //     })
 
-//     it (`LP09-Verify that the user is able to navigate to "Username" field by pressing "TAB" key on the keyboard.`, ()=> {
+//     it(`LP07-Verify that the user is able to navigate to "Username" field by pressing "TAB" key on the keyboard.`, ()=> {
 //         cy.get('body')
 //             .tab()
 
 //         cy.focused().should('have.id','username-field')
 //     })
 
-//     it (`LP10-Verify that the user is able to navigate to "Password" field by pressing "TAB" key on the keyboard.`, ()=> {
+//     it(`LP08-Verify that the user is able to navigate to "Password" field by pressing "TAB" key on the keyboard.`, ()=> {
 //         cy.get('body')
 //             .tab()
 //             .tab()
@@ -77,7 +81,7 @@ describe(`Verify that all the labels and controls including text-boxes, buttons,
 //         cy.focused().should('have.id', 'password-field')
 //     })
 
-//     it (`LP11-Verify that the user is able to navigate to "Login" button by pressing "TAB" key on the keyboard.`, ()=> {
+//     it (`LP09-Verify that the user is able to navigate to "Login" button by pressing "TAB" key on the keyboard.`, ()=> {
 //         cy.get('body')
 //             .tab()
 //             .tab()
@@ -86,71 +90,69 @@ describe(`Verify that all the labels and controls including text-boxes, buttons,
 //         cy.focused().should('have.id', 'login-form-submit')
 //     })
 
-//     it (`LP12-Verifying that  the password as it is being typed in is being hashed and can't be copied`, ()=> {
+//     it (`LP10-Verifying that  the password as it is being typed in is being hashed and can't be copied`, ()=> {
 //         cy.get('#password-field')
 //             .invoke('attr', 'type')
 //             .should('contain', 'password') 
 //     })
 
-//     it (`LP13-Verify that the user is able to login by entering valid credentials and clicking on the ‘Login’ button.`, () => {
+//     it (`LP11-Verify that the user is able to login by entering valid credentials and clicking on the ‘Login’ button.`, () => {
 
 //         enterLoginDetails("user", "password")
 
 //         cy.get('#login-form-submit').click()
 
 //         cy.eyesCheckWindow({
-//             tag: "Login Page-Succefull Login-With Login Button",
+//             tag: "LP13-Verify that the user is able to login by entering valid credentials and clicking on the ‘Login’ button.",
 //             target: 'window',
 //             fully: true
 //         });
 //     })
 
-//     it (`LP14-Verify that the user is able to login by entering valid credentials and pressing Enter key.`, () => {
+//     it (`LP12-Verify that the user is able to login by entering valid credentials and pressing Enter key.`, () => {
 
 //         enterLoginDetails("user", "password")
 
 //         cy.focused().type('{enter}')
 
 //         cy.eyesCheckWindow({
-//             tag: "Login Page-Sucessfull Login-With Enter Button",
+//             tag: "LP14-Verify that the user is able to login by entering valid credentials and pressing Enter key.",
 //             target: 'window',
 //             fully: true
 //         });
 //     })
 
-//     it (`LP15-Verify that the user is NOT able to login by entering INVALID credentials and clicking on the ‘Login’ button.`, () => {
+//     it (`LP13-Verify that the user is NOT able to login by entering INVALID credentials and clicking on the ‘Login’ button.`, () => {
 
 //         enterLoginDetails("bla", "blabla")
 
 //         cy.get('#login-form-submit').click()
 //         cy.eyesCheckWindow({
-//             tag: "Verify that the user is NOT able to login by entering INVALID credentials and clicking on the ‘Login’ button",
+//             tag: "LP15-Verify that the user is NOT able to login by entering INVALID credentials and clicking on the ‘Login’ button.",
 //             target: 'window',
 //             fully: true
 //         });
 //     })
 
-//     it (`LP16-Verify that the validation message gets displayed in case the user leaves the username field as blank and that the message does not indicate which is wrong or empty.`, () => {
+//     it (`LP14-Verify that the validation message gets displayed in case the user leaves the username field as blank and that the message does not indicate which is wrong or empty.`, () => {
 
 //         enterLoginDetails("user", "")
 
 //         cy.get('#login-form-submit').click()
 
 //         cy.eyesCheckWindow({
-//             tag: "Verify that the validation message gets displayed in case the user leaves the username field as blank and that the message does not indicate which is wrong or empty.",
+//             tag: "LP16-Verify that the validation message gets displayed in case the user leaves the username field as blank and that the message does not indicate which is wrong or empty.",
 //             target: 'window',
 //             fully: true
 //         });
 //     })
 
-//     it (`LP17-Verify that the validation message gets displayed in case the user leaves the password field as blank and that the message does not indicate which is wrong or empty`, () => {
+//     it (`LP15-Verify that the validation message gets displayed in case the user leaves the password field as blank and that the message does not indicate which is wrong or empty.`, () => {
 
 //         enterLoginDetails("", "password")
-
 //         cy.get('#login-form-submit').click()
-
 //         cy.eyesCheckWindow({
-//             tag: "Verify that the validation message gets displayed in case the user leaves the password field as blank and that the message does not indicate which is wrong or empty",
+//             tag: "LP17-Verify that the validation message gets displayed in case the user leaves the password field as blank and that the message does not indicate which is wrong or empty.",
 //             target: 'window',
 //             fully: true
 //         });
