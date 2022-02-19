26cd34f75f4521e6f8406e679752cde336a2a9e0
Rad Lisowski <radlisowski@gmail.com>
2022-02-19 15:31:10 +0000

    final version of te tests working - no flakyness:D

--- cypress/integration/login-test.spec.js
+++ cypress/integration/login-test.spec.js
@@ -2,10 +2,10 @@
 /// <reference types="@applitools/eyes-cypress" />
 
 describe(`Verify that all the labels and controls including text-boxes, buttons, and links are present on the Login page and can be interacted with.`, () => {
-    
+
     beforeEach(() => {
         cy.visit('https://radlisowski.github.io/login.html')
-        
+
         cy.eyesOpen({
             appName: 'Login Page',
             testName: 'Verify that all the labels and controls including text-boxes, buttons, and links are present on the Login page and can be interacted with',
@@ -17,8 +17,8 @@ describe(`Verify that all the labels and controls including text-boxes, buttons,
     });
 
     //********tests-cases*********
-    
-    it(`LP01-Verifying visibility of the elements on the page` , ()=> {
+
+    it(`LP01-Verifying visibility of the page objects`, () => {
         cy.eyesCheckWindow({
             tag: "LP01-Verifying visibility of the elements on the page",
             target: 'window',
@@ -26,150 +26,123 @@ describe(`Verify that all the labels and controls including text-boxes, buttons,
         });
     })
 
-    it(`LP02-Verifying Username text-box can take text input` , ()=> {
+    it(`LP02-Verifying Username text-box can take text input`, () => {
         cy.get('#username-field')
             .type("test")
-            .should('have.value', 'test')    
+            .should('have.value', 'test')
     })
 
-    it(`LP03-Verifying Password text-box can take text input` , ()=> {
+    it(`LP03-Verifying Password text-box can take text input`, () => {
         cy.get('#password-field')
             .type("test")
-            .should('have.value', 'test')    
+            .should('have.value', 'test')
     })
 
-    it(`LP04-Verifying Login button can be pressed` , ()=> {
+    it(`LP04-Verifying Login button can be pressed`, () => {
         cy.get('#signin-button').click()
 
-        cy.get('#error-button').should('have.text', 'Wrong Username or Password')    
+        cy.get('#error-button').should('have.text', 'Wrong Username or Password')
     })
- })
+})
 
-// describe(`LP05Verify various user interaction on the Login Page (positive/negative).`, () => {
-    
-//     beforeEach(() => {
-//         cy.visit('https://radlisowski.github.io//login-test-page/index.html')
-        
-//         cy.eyesOpen({
-//             appName: 'Login Page',
-//             testName: 'Verify various user interaction on the Login Page (positive/negative)',
-//         })
-//     })
+describe(`Verify various user interaction on the Login Page (positive/negative).`, () => {
 
-//     afterEach(() => {
-//         cy.eyesClose();
-//     });
+    beforeEach(() => {
+        cy.visit('https://radlisowski.github.io/login.html')
 
-//     //********tests-cases*********
+    })
 
-//     it(`LP06-Verify that as soon as the login page opens, by default the cursor should remain on the username textbox.` , ()=> {
-//         cy.focused().should('have.id', 'username-field');
-//     })
+    //********tests-cases*********
 
-//     it(`LP07-Verify that the user is able to navigate to "Username" field by pressing "TAB" key on the keyboard.`, ()=> {
-//         cy.get('body')
-//             .tab()
+    it(`LP05-Verify that as soon as the login page opens, by default the cursor should remain on the username textbox.`, () => {
+        cy.focused().should('have.id', 'username-field');
+    })
 
-//         cy.focused().should('have.id','username-field')
-//     })
+    it(`LP06-Verify that the user is able to navigate to "Username" field by pressing "TAB" key on the keyboard.`, () => {
+        cy.get('body')
+            .tab()
 
-//     it(`LP08-Verify that the user is able to navigate to "Password" field by pressing "TAB" key on the keyboard.`, ()=> {
-//         cy.get('body')
-//             .tab()
-//             .tab()
+        cy.focused().should('have.id', 'username-field')
+    })
 
-//         cy.focused().should('have.id', 'password-field')
-//     })
+    it(`LP07-Verify that the user is able to navigate to "Password" field by pressing "TAB" key on the keyboard.`, () => {
+        cy.get('body')
+            .tab()
+            .tab()
 
-//     it (`LP09-Verify that the user is able to navigate to "Login" button by pressing "TAB" key on the keyboard.`, ()=> {
-//         cy.get('body')
-//             .tab()
-//             .tab()
-//             .tab()
+        cy.focused().should('have.id', 'password-field')
+    })
 
-//         cy.focused().should('have.id', 'login-form-submit')
-//     })
+    it(`LP08-Verify that the user is able to navigate to "Login" button by pressing "TAB" key on the keyboard.`, () => {
+        cy.get('body')
+            .tab()
+            .tab()
+            .tab()
 
-//     it (`LP10-Verifying that  the password as it is being typed in is being hashed and can't be copied`, ()=> {
-//         cy.get('#password-field')
-//             .invoke('attr', 'type')
-//             .should('contain', 'password') 
-//     })
+        cy.focused().should('have.id', 'signin-button')
+    })
 
-//     it (`LP11-Verify that the user is able to login by entering valid credentials and clicking on the ‘Login’ button.`, () => {
+    it(`LP09-Verifying that  the password as it is being typed in is being hashed and can't be copied`, () => {
+        cy.get('#password-field').invoke('attr', 'type')
+            .should('contain', 'password')
+    })
 
-//         enterLoginDetails("user", "password")
+    it(`LP10-Verify that the user is able to login by entering valid credentials and clicking on the ‘Login’ button.`, () => {
 
-//         cy.get('#login-form-submit').click()
+        enterLoginDetails("test", "test")
 
-//         cy.eyesCheckWindow({
-//             tag: "LP13-Verify that the user is able to login by entering valid credentials and clicking on the ‘Login’ button.",
-//             target: 'window',
-//             fully: true
-//         });
-//     })
+        cy.get('#signin-button').click()
 
-//     it (`LP12-Verify that the user is able to login by entering valid credentials and pressing Enter key.`, () => {
+        cy.get('#success-button').should('have.text', 'Cool Beans!')
+    })
 
-//         enterLoginDetails("user", "password")
+    it(`LP11-Verify that the user is able to login by entering valid credentials and pressing Enter key.`, () => {
 
-//         cy.focused().type('{enter}')
+        enterLoginDetails("test", "test")
 
-//         cy.eyesCheckWindow({
-//             tag: "LP14-Verify that the user is able to login by entering valid credentials and pressing Enter key.",
-//             target: 'window',
-//             fully: true
-//         });
-//     })
+        cy.focused().type('{enter}')
 
-//     it (`LP13-Verify that the user is NOT able to login by entering INVALID credentials and clicking on the ‘Login’ button.`, () => {
+        cy.get('#success-button').should('have.text', 'Cool Beans!')
+    })
 
-//         enterLoginDetails("bla", "blabla")
+    it(`LP12-Verify that the user is NOT able to login by entering INVALID credentials and clicking on the ‘Login’ button.`, () => {
 
-//         cy.get('#login-form-submit').click()
-//         cy.eyesCheckWindow({
-//             tag: "LP15-Verify that the user is NOT able to login by entering INVALID credentials and clicking on the ‘Login’ button.",
-//             target: 'window',
-//             fully: true
-//         });
-//     })
+        enterLoginDetails("bla", "blabla")
 
-//     it (`LP14-Verify that the validation message gets displayed in case the user leaves the username field as blank and that the message does not indicate which is wrong or empty.`, () => {
+        cy.get('#signin-button').click()
 
-//         enterLoginDetails("user", "")
+        cy.get('#error-button').should('have.text', 'Wrong Username or Password')
+    })
 
-//         cy.get('#login-form-submit').click()
+    it(`LP13-Verify that the validation message gets displayed in case the user leaves the username field as blank and that the message does not indicate which is wrong or empty.`, () => {
 
-//         cy.eyesCheckWindow({
-//             tag: "LP16-Verify that the validation message gets displayed in case the user leaves the username field as blank and that the message does not indicate which is wrong or empty.",
-//             target: 'window',
-//             fully: true
-//         });
-//     })
+        enterLoginDetails("", "test")
 
-//     it (`LP15-Verify that the validation message gets displayed in case the user leaves the password field as blank and that the message does not indicate which is wrong or empty.`, () => {
+        cy.get('#signin-button').click()
 
-//         enterLoginDetails("", "password")
-//         cy.get('#login-form-submit').click()
-//         cy.eyesCheckWindow({
-//             tag: "LP17-Verify that the validation message gets displayed in case the user leaves the password field as blank and that the message does not indicate which is wrong or empty.",
-//             target: 'window',
-//             fully: true
-//         });
-//     })
-// })
+        cy.get('#error-button').should('have.text', 'Wrong Username or Password')
+    })
 
-// function enterLoginDetails(username, thePassword) {
-//     //have to do this check as .type does not take empty strings
-//     if (! username == '') {
-//         cy.get('#username-field')
-//         .type(username)
-//         .should('have.value', username)
-//     } 
-    
-//     if (! thePassword == '') {
-//         cy.get('#password-field')
-//         .type(thePassword)
-//         .should('have.value', thePassword)
-//     } 
-// }
+    it(`LP15-Verify that the validation message gets displayed in case the user leaves the password field as blank and that the message does not indicate which is wrong or empty.`, () => {
+
+        enterLoginDetails("", "password")
+        cy.get('#signin-button').click()
+
+        cy.get('#error-button').should('have.text', 'Wrong Username or Password')
+    })
+})
+
+function enterLoginDetails(username, thePassword) {
+    //have to do this check as .type does not take empty strings
+    if (!username == '') {
+        cy.get('#username-field')
+            .type(username)
+            .should('have.value', username)
+    }
+
+    if (!thePassword == '') {
+        cy.get('#password-field')
+            .type(thePassword)
+            .should('have.value', thePassword)
+    }
+}
